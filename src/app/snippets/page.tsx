1845c6af05dd49d176790748238b2d447a746029
minaal07 <130892773+minaal07@users.noreply.github.com>
2025-04-02 17:18:42 +0530

    feat(ui): add SnippetsPageSkeleton component

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
 
-const Snippets = () => {
-  return (
-    <div>Snippets</div>
-  )
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
+import { div } from "framer-motion/client";
+import SnippetsPageSkeleton from "./_components/SnippetsPageSkeleton";
+
+function SnippetsPage() {
+    const snippets = useQuery(api.snippets.getSnippets);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedLanguage, setSelectedLanguage] = useState<string | null>(
+        null
+    );
+    const [view, setView] = useState<"grid" | "list">("grid");
+
+    // loading state
+    if (snippets === undefined) {
+        return (
+            <div className="min-h-screen">
+                <NavigationHeader />
+                <SnippetsPageSkeleton />
+            </div>
+        );
+    }
+
+
+    return (
+        <div></div>
+    );
 }
-
-export default Snippets
+export default SnippetsPage;
