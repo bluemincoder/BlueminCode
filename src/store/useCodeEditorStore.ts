35d4782f21ae6b64842935b11e04c6f72371d7d1
minaal07 <130892773+minaal07@users.noreply.github.com>
2025-04-09 19:48:41 +0530

    chore: update default coding language

--- src/store/useCodeEditorStore.ts
+++ src/store/useCodeEditorStore.ts
@@ -7,15 +7,15 @@ const getInitialState = () => {
     // if we're on the server, return default values
     if (typeof window === "undefined") {
         return {
-            language: "javascript",
+            language: "cpp",
             fontSize: 16,
-            theme: "vs-dark",
+            theme: "github-dark",
         };
     }
 
     // if we're on the client, return values from local storage bc localStorage is a browser API.
     const savedLanguage =
-        localStorage.getItem("editor-language") || "javascript";
+        localStorage.getItem("editor-language") || "cpp";
     const savedTheme = localStorage.getItem("editor-theme") || "vs-dark";
     const savedFontSize = localStorage.getItem("editor-font-size") || 16;
 
